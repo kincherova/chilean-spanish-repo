0db41cf80c731390c3ef4d5922d97c9250fd04b6
kincherova <kincherova@gmail.com>
2026-03-09 12:46:12 -0300

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/survival-chilean-spanish/',
+  base: '/survival-chilean-spanish-2/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
